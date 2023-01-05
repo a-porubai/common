55b3347b5f864ea61b8fca21a78a613d2f183ed9
a-porubai <89394217+a-porubai@users.noreply.github.com>
2023-01-05 15:21:53 +0200

    Update service.go

--- internal/core/service/gamesrv/service.go
+++ internal/core/service/gamesrv/service.go
@@ -1,14 +1,5 @@
 package gamesrv
 
-import (
-	"errors"
-
-	"github.com/matiasvarela/minesweeper-hex-arch-sample/internal/core/domain"
-	"github.com/matiasvarela/minesweeper-hex-arch-sample/internal/core/ports"
-	"github.com/matiasvarela/minesweeper-hex-arch-sample/pkg/apperrors"
-	"github.com/matiasvarela/minesweeper-hex-arch-sample/pkg/uidgen"
-)
-
 type Service struct {
 	gamesRepository ports.GamesRepository
 	uidGen          uidgen.UIDGen
